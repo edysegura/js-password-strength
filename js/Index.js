9952ed34a228d5bf92cfdaf535eba35a760831ba
Edy Segura <edysegura@dodson.dreamhost.com>
2017-01-11 23:24:31 -0200

    First commit as baseline

---
+++ js/Index.js
@@ -0,0 +1,50 @@
+﻿/**
+ *
+ * Força da Senha
+ * @author Edy Segura, edy@segura.pro.br
+ *
+ */
+
+var Index = {
+
+	init: function() {
+		Index.setInputPassword();
+	},
+	
+	
+	setInputPassword: function() {
+		var input = document.getElementById("password");
+		
+		input.onkeyup = function() {
+			Index.showMessage(this.value);
+		}
+	},
+	
+	
+	showMessage: function(password) {
+		var message = document.getElementById("message");
+		message.innerHTML = Pwd.getInfo(password);
+		
+		switch(Pwd.check(password)) {
+			case -3:
+				message.className = "muito-baixa";
+			break;
+			
+			case -2:
+				message.className = "baixa";
+			break;
+			
+			case -1:
+				message.className = "bom";
+			break;
+			
+			case 0:
+				message.className = "excelente";
+			break;
+		}
+	}
+	
+}
+
+//inicializacao
+window.onload = Index.init;
---
+++ js/Pwd/Pwd.js
@@ -0,0 +1,50 @@
+﻿/**
+ *
+ * Força da Senha
+ * @author Edy Segura, edy@segura.pro.br
+ *
+ */
+
+var Pwd = {
+
+	getInfo: function(password) {
+		return Pwd.getMessage(Pwd.check(password));
+	},
+	
+	
+	check: function(password) {
+		var points = 0;
+		
+		if(password.length < 6) points = points - 1;
+		if(!password.match(/[a-z_]/i) || !password.match(/[0-9]/)) points = points - 1;
+		if(!password.match(/\W/)) points = points - 1;
+		
+		return points;
+	},
+	
+	
+	getMessage: function(messageNumber) {
+		var message = "";
+		
+		switch(messageNumber) {
+			case -3:
+				message = "MUITO BAIXA";
+			break;
+			
+			case -2:
+				message = "BAIXA";
+			break;
+			
+			case -1:
+				message = "BOM";
+			break;
+			
+			case 0:
+				message = "EXCELENTE";
+			break;
+		}
+		
+		return message;
+	}
+	
+}
